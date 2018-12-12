b97d24265f7de9ecb0eb65fbc05eb22a33b2af5e
hiroki inoue <hiroki12malu@gmail.com>
2018-12-12 16:56:29 +0900

    only picking up different sex

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
   def index
-    @users = User.where.not(id: current_user.id)
+    all_sex_users = User.where.not(id: current_user.id)
+    @users = all_sex_users.select do |other|
+      current_user.sex != other.sex
+    end
   end
 
   def show
